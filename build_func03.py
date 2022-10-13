b1efa8768439b9f27e91a45dd07dda4f0fe20492
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 16:03:00 +0500

    uchinchi3

--- build_func03.py
+++ build_func03.py
@@ -1,11 +1,12 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func03
-from math import pow
+
 n=3.5
 n=n+1
 n=pow(n,2)
 answer=3*n
 print(answer)
+
 
 
 
