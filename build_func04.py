d64cac1e73a3d716f4c380e9f746553682b9d902
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 16:51:13 +0500

    turtinchi

--- build_func04.py
+++ build_func04.py
@@ -1,10 +1,8 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func04
-from math import pow
-n=3.5
-n=n+1
-n=pow(n,2)
-answer=3*n
+n=4
+answer=pow(((n+2)/3),2)
+
 print(answer)
 
 
