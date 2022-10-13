0864dcd4b0147fd398de98e8b2bdd089109df07f
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 13:58:59 +0500

    oltinchisi6

--- build_func06.py
+++ build_func06.py
@@ -1,2 +1,12 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func06
+a=3.456
+answer=round(3.456,2)
+print(answer)
+a=7.5
+answer=round(7.5,1)
+print(answer)
+
+
+
+
