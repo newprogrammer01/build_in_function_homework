9ce5b815315debead1f1988eff66ad89263436fc
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 10:45:19 +0500

    ikkinchisi2

--- build_func02.py
+++ build_func02.py
@@ -1,2 +1,8 @@
 # the task condition is placed in the README file
-#ttps://github.com/codeschool43/Build_in_function_homework#build_func02
+#ttps://github.com/codeschool43/Build_in_function_homework#build_func
+
+
+
+answer=3*(7/5-9/4)
+print(answer)
+
