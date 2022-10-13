e5434ec636a6fdf4becf1f9307d6f85bf27ec2a9
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 16:54:23 +0500

    ikkinchi2222222

--- build_func02.py
+++ build_func02.py
@@ -1,6 +1,8 @@
 # the task condition is placed in the README file
 #ttps://github.com/codeschool43/Build_in_function_homework#build_func
 
-answer=3*(7/5-9/4)
+a=3*(7/5-9/4)
+answer=a
 
 print(answer)
+
