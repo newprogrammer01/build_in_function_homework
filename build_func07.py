db3c9fc48a2b850a4b5b80c91c8de41e7f35ddbc
newprogrammer01 <jasurbekpirnazarov09@gmail.com>
2022-10-13 14:06:35 +0500

    yettinchisi7

--- build_func07.py
+++ build_func07.py
@@ -1,2 +1,10 @@
 # the task condition is placed in the README file
 # https://github.com/codeschool43/Build_in_function_homework#build_func07
+'''x kvadrat +6x kub+3xy x=5 y=2  '''
+x=5
+x1=pow(x,2)
+x2=pow(x,3)
+y=2
+answer=x1+6*x2+3*x*y
+print(answer)
+
